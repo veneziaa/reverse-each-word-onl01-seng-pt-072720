469aa6d3df105716e43a526f88227de97e3bf001
Venice Vargas <venicevargas1@live.com>
2020-08-08 18:46:37 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(string)
-  array = string.split(" ") #turn string into an array
+  array = string.split(" ") 
   test_array = []
-  array.collect do|string| #iterate over the array
+  array.collect do|string| 
     test_array << string.reverse 
   end
   test_array.join(" ")

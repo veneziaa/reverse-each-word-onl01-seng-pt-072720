b538bbb92b2fbdf00c17a7e833c01ac416d531f6
Venice Vargas <venicevargas1@live.com>
2020-08-08 18:43:12 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,8 @@
 def reverse_each_word(sentence)
-  reversed = sentence.split(" ").collect do |word|
+  reversed = sentence.split("Hello there, and how are you?").collect do |word|
   word.reverse
 end
-  reversed.join(" ")
+  reversed.join("Hello there, and how are you?")
 end
   
-  Hi again, just making sure it's reversed!
+  

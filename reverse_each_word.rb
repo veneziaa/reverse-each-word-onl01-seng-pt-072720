6d0b9fc4ecd34ff88e6a4826ebfe701b233b4547
Venice Vargas <venicevargas1@live.com>
2020-08-08 18:46:28 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,4 +1,11 @@
-
+def reverse_each_word(string)
+  array = string.split(" ") #turn string into an array
+  test_array = []
+  array.collect do|string| #iterate over the array
+    test_array << string.reverse 
+  end
+  test_array.join(" ")
+end
 reverse_each_word("Hello there, and how are you?")
   
   
